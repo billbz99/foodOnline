cb403eb7d51a844f28a39a1e8471999ce9544dfb
Guillermo Baquerizo <guillemo_baquerizo@optum.com>
2024-09-09 14:20:25 -0400

    Adding constants

---
+++ static/benefits-api/constants/ApplicationConstants.java
@@ -0,0 +1,20 @@
+package com.optum.geba.benefitsapi.constants;
+
+public class ApplicationConstants {
+    public static final String H2_CONSOLE_PATH = "/h2-console/**";
+    public static final String SIGNIN_PATH = "/signin";
+    public static final String HELLO_PATH = "/hello";
+    public static final String USER_PATH = "/user";
+    public static final String ADMIN_PATH = "/admin";
+    public static final String USER1_USERNAME = "user1";
+    public static final String USER1_PASSWORD = "password1";
+    public static final String ADMIN_USERNAME = "admin";
+    public static final String ADMIN_PASSWORD = "adminPass";
+    public static final String USER_ROLE = "USER";
+    public static final String ADMIN_ROLE = "ADMIN";
+    public static final String BAD_CREDENTIALS = "Bad credentials";
+    public static final String UNAUTHORIZED = "Unauthorized";
+    public static final String JWT_HEADER = "Authorization";
+    public static final String JWT_BEARER_TOKEN_STRING = "Bearer ";
+
+}
